bf44b1693c75c1baa108f35cef3ac69b1342bce2
lobosz <di.loboso@duocuc.cl>
2026-08-04 11:42:44 -0400

    security config

---
+++ src/main/java/com/diego/Clasificador_gastos/ClasificadorGastosApplication.java
@@ -0,0 +1,13 @@
+package com.diego.Clasificador_gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasificadorGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasificadorGastosApplication.class, args);
+	}
+
+}
